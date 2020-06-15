9174262fa28d85fa7f3d2f4f61d7201fd560ec5c
Nicole Wong <nrewong@gmail.com>
2020-06-15 11:00:15 +0900

    monkey patch ImageAnalyzer to add exif to metadata column on ActiveStorage blobs

---
+++ config/initializers/exif_analyzer.rb
@@ -0,0 +1,18 @@
+module ActiveStorage
+  class Analyzer::ImageAnalyzer < Analyzer
+
+    def metadata
+      read_image do |image|
+        if rotated_image?(image)
+          { width: image.height, height: image.width }
+        else
+          { width: image.width, height: image.height }
+        end.merge(image.exif || {})
+      end
+    rescue LoadError
+      logger.info "Skipping image analysis because the mini_magick gem isn't installed"
+      {}
+    end
+
+  end
+end
