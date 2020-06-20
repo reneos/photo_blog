d5f78e79a3002bf2acb045e1d77d7f39f7c12056
Nicole Wong <nrewong@gmail.com>
2020-06-20 18:30:02 +0900

    update exif display

--- config/initializers/exif_analyzer.rb
+++ config/initializers/exif_analyzer.rb
@@ -1,15 +1,4 @@
 module ActiveStorage
-  # Extracts width and height in pixels from an image blob.
-  #
-  # If the image contains EXIF data indicating its angle is 90 or 270 degrees, its width and height are swapped for convenience.
-  #
-  # Example:
-  #
-  #   ActiveStorage::Analyzer::ImageAnalyzer.new(blob).metadata
-  #   # => { width: 4104, height: 2736 }
-  #
-  # This analyzer relies on the third-party {MiniMagick}[https://github.com/minimagick/minimagick] gem. MiniMagick requires
-  # the {ImageMagick}[http://www.imagemagick.org] system library.
   class Analyzer::ImageAnalyzer < Analyzer
 
     def metadata
@@ -40,17 +29,6 @@ module ActiveStorage
           attributes[property] = exif[value]
         end
         @blob.update(attributes)
-      end
-
-      def read_image
-        download_blob_to_tempfile do |file|
-          require "mini_magick"
-          yield MiniMagick::Image.new(file.path)
-        end
-      end
-
-      def rotated_image?(image)
-        %w[ RightTop LeftBottom ].include?(image["%[orientation]"])
       end
   end
 end
