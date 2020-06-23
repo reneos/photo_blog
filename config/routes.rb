b415fa44718c1327c46a8b64c0388821e13cdca8
Nicole Wong <nrewong@gmail.com>
2020-06-23 18:13:38 +0900

    change devise to allow only one user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   resources :posts, only: [:index, :update, :edit, :create, :show] do
     member do
