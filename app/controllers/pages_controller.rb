337a4e059caf082977aab6dd3646ab8f98cf9fda
Nicole Wong <nrewong@gmail.com>
2020-06-24 20:32:27 +0900

    add home page image, make logo image smaller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @posts = policy_scope(Post).published.first(6)
+    @posts = policy_scope(Post).published.order(post_date: :desc).first(6)
   end
 
   def stats
